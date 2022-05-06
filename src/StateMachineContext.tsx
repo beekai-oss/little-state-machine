9f0e8ff29690b666e140da93efbff4dbe42ecdc8
Almusweb <Almusweb@users.noreply.github.com>
2022-05-06 08:47:59 +0200

    Upgrading for React 18 with TypeScript
    fixing React type definitionss -> Removal of implicit children
    
    You can run:
    npx types-react-codemod preset-18 ./src

--- src/StateMachineContext.tsx
+++ src/StateMachineContext.tsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 import storeFactory from './logic/storeFactory';
 import { StateMachineContextValue } from './types';
 
+type PropsChildren = {
+    children?: React.ReactNode
+};
+
 const StateMachineContext = React.createContext<StateMachineContextValue>(undefined as any);
 
-export const StateMachineProvider: React.FC = ({ children }) => {
+export const StateMachineProvider: React.FC<PropsChildren> = ({ children }) => {
   const [state, setState] = React.useState(storeFactory.state);
 
   return (
