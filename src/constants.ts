8b99fc709d993e2bf479249ad445e4aec371b1c5
Bill <bluebill1049@hotmail.com>
2022-09-17 08:44:19 +1000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const STORE_DEFAULT_NAME = '__LSM__';
 export const STORE_ACTION_NAME = '__LSM_NAME__';
 export const PERSIST_OPTION = {
   NONE: 'none',
-  ACTION: 'onAction',
+  ACTION: 'action',
   UNLOAD: 'beforeUnload',
 } as const;
