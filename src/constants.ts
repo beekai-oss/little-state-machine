bf189d570154f4e83b8e07002cadee5d7ae4f4a1
Szymon Sus <sus.szymon3@gmail.com>
2022-09-17 00:47:15 +0200

    feat: introduce disable persist option ("none")  (#137)
    * fix: persist option none is not working
    
    * refactor: save some bytes
    
    - remove unnecessary enum export
    - use const object instead of enum
    - avoid using optional chain operator
    
    * chore(deps): bump microbundle
    
    - saves another 200B gzipped
    
    * docs: add persist option to API documentation
    
    * Update README.md
    
    * Update constants.ts
    
    Co-authored-by: Bill <bluebill1049@hotmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const STORE_DEFAULT_NAME = '__LSM__';
 export const STORE_ACTION_NAME = '__LSM_NAME__';
-export const PERSIST_BEFORE_UNLOAD = 'beforeUnload';
+export const PERSIST_OPTION = {
+  NONE: 'none',
+  ACTION: 'action',
+  UNLOAD: 'beforeUnload',
+} as const;
